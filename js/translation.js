6c8e8f77699dfed4197fd67cc2ed20e3a693ca48
zMohaIQ <gbyt01142@gmail.com>
2023-01-07 16:08:58 +0200

    Added-About-Translates

--- js/translation.js
+++ js/translation.js
@@ -1,5 +1,6 @@
 const translations = {
   en: {
+    // Index HTML
     home: `Home`,
     gallery: `Gallery`,
     products: `Products`,
@@ -8,8 +9,39 @@ const translations = {
     quote_welcome: `Welcome to,`,
     quote_p: `Arabian Rubber Company and learn how to request your order of rubber tiles and Reclaimed rubber from recycled tires. Reclaimed rubber is manufactured with unprecedented quality, opening up a whole new world of interesting application prospects.`,
     logo: `RUBBER FLOORING TILES`,
+    // About HTML
+    about_intro: `INTRODUCTION`,
+    about_intro_text: `Established in 2013, Arabian Co for rubber (shorted as ACR) The Rubber Company has a vast wealth of experience, working with customers in almost
+            every rubber product and providing an extensive range of different solutions. We are the experts in rubber. Based in the Egypt and working with
+            companies worldwide.`,
+    about_us: `ABOUT US`,
+    about_us_text: `It is a long established fact that a reader will be distracted by the`,
+    about_quailty_control: `OUR QUALITY CONTROL`,
+    about_quailty_control_text: `The well-controlled should product quality is the main secret of Arabian Co for rubber's 10 years business. This quality-control policy has
+                been. We had got the Reach Directive (EC)1907/2006 Certificate from AQSR in UK for many years, and strictly apply these quality management
+                systems in each day production. We also had got ISO 28540:2011 Certificate from AQSR since 2020 We also get ROHS Directive EU2015/863
+                Certificate.`,
+    about_oversea_business: `OUR OVERSEA BUSINESS`,
+    about_oversea_business_text: `We are not only satisfied with our increasing domestic market share, and we want to introduce our outstanding products to the whole world. Since
+                2014 doing oversea trading, now we have many clients and business partners all over the world.`,
+    about_core_value: `OUR CORE VALUE`,
+    about_core_value_text: `Outstanding product quality, sincerely customer service and professional technical support. " Is our company core value and rule of business.
+                "Providing outstanding rubber products to benefit and serve the society and whole world forever" is our company mission forever. Our experienced
+                staff members are always willing and happy to discuss your requirements and ensure full customer satisfaction.`,
+    about_our_mission: `OUR MISSION`,
+    about_our_mission_text: `Arabian Rubber Company was established to continue on the basis of development, commitment and helping customers to achieve trust between it and
+                contracting companies, and to seek to achieve a great position in the global market and obtain exclusive agencies to supply rubber materials
+                necessary for manufacturing, secure long-term relationships with international companies, and provide various services to the commercial sector
+                in various countries of the world. Our long-term vision is building a conglomerate that operates internationally.`,
+    about_our_strategy: `OUR STRATEGY`,
+    about_our_strategy_text: `Our company strategy is concerned with caring about our clients, carefully looking at their needs, developing our services, being sustainable,
+                reliable and excelling at being a reliable partner.`,
+    about_our_vision: `OUR VISION`,
+    about_our_vision_text: `Our vision is to maintain reliable and high-quality suppliers of high-quality materials on a regular basis which form the basis for building
+                long-term partnerships, and to take care of our company to maintain market demands.`,
   },
   ar: {
+    // Index HTML
     home: `الصفحة الرئسية`,
     gallery: `الصور`,
     products: `المنتجات`,
@@ -18,6 +50,24 @@ const translations = {
     quote_welcome: `مرحبا بكم`,
     quote_p: `الشركة العربية للمطاط وتعلم كيفية طلب طلبك من البلاط المطاطي والمطاط المستصلح من الإطارات المعاد تدويرها. يتم تصنيع المطاط المستصلح بجودة غير مسبوقة ، مما يفتح عالما جديدا تماما من آفاق التطبيق المثيرة للاهتمام.`,
     logo: `الأرضيات المطاطية`,
+    // About HTML
+    about_intro: `المقدمة`,
+    about_intro_text: `تأسست الشركة العربية للمطاط في عام 2013 ، ولديها ثروة واسعة من الخبرة ، حيث تعمل مع العملاء في كل منتج من منتجات المطاط تقريبا وتوفر مجموعة واسعة من الحلول المختلفة.
+نحن خبراء في المطاط. مقرها في مصر وتعمل مع الشركات في جميع أنحاء العالم .`,
+    about_us: `معلومات عنا`,
+    about_us_text: `إنها حقيقة راسخة منذ فترة طويلة أن القارئ سوف يشتت انتباهه بسبب`,
+    about_quailty_control: `مراقبة الجودة لدينا`,
+    about_quailty_control_text: `إن جودة المنتج التي يتم التحكم فيها جيدا هي السر الرئيسي للشركة العربية لأعمال المطاط لمدة 10 سنوات. كانت سياسة مراقبة الجودة هذه. لقد حصلنا على شهادة Reach Directive (EC) 1907/2006 من AQSR في المملكة المتحدة لسنوات عديدة، ونطبق بدقة أنظمة إدارة الجودة هذه في كل إنتاج يومي. لقد حصلنا أيضا على شهادة ISO 28540: 2011 من AQSR منذ عام 2020 ، كما حصلنا على شهادة توجيه ROHS EU2015 / 863 .`,
+    about_oversea_business: `أعمالنا من الخارج`,
+    about_oversea_business_text: `نحن لسنا راضين فقط عن حصتنا المتزايدة في السوق المحلية ، ونريد أن نقدم منتجاتنا المتميزة للعالم بأسره. منذ عام 2014 القيام بالتداول من الخارج ، لدينا الآن العديد من العملاء والشركاء التجاريين في جميع أنحاء العالم .`,
+    about_core_value: `قيمنا الأساسية`,
+    about_core_value_text: `قيمتنا الأساسية: "جودة المنتج المتميزة ، خدمة العملاء بصدق والدعم الفني المهني. " هي القيمة الأساسية لشركتنا وقاعدة العمل. "توفير منتجات المطاط المتميزة لصالح وخدمة المجتمع والعالم كله إلى الأبد" هي مهمة شركتنا إلى الأبد. موظفونا ذوو الخبرة مستعدون دائما وسعداء لمناقشة متطلباتك وضمان رضا العملاء الكامل .`,
+    about_our_mission: `مهمتنا`,
+    about_our_mission_text: `تأسست الشركة العربية للمطاط لتستمر على أساس التطوير والالتزام ومساعدة العملاء على تحقيق الثقة بينها وبين شركات المقاولات ، والسعي لتحقيق مكانة كبيرة في السوق العالمية والحصول على وكالات حصرية لتوريد المواد المطاطية اللازمة للتصنيع ، وتأمين علاقات طويلة الأمد مع الشركات العالمية ،، وتقديم الخدمات المختلفة للقطاع التجاري في مختلف دول العالم. رؤيتنا طويلة الأجل هي بناء تكتل يعمل دوليا. .`,
+    about_our_strategy: `استراتيجيتنا`,
+    about_our_strategy_text: `تهتم استراتيجية شركتنا بالاهتمام بعملائنا ، والنظر بعناية في احتياجاتهم ، وتطوير خدماتنا ، والاستدامة والموثوقية والتفوق في كوننا شريكا موثوقا به .`,
+    about_our_vision: `رؤيتنا`,
+    about_our_vision_text: `تتمثل رؤيتنا في الحفاظ على موردين موثوقين وذوي جودة عالية للمواد عالية الجودة على أساس منتظم والتي تشكل الأساس لبناء شراكات طويلة الأجل ، ورعاية شركتنا للحفاظ على متطلبات السوق .`,
   },
 };
 
